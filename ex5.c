eefd2e5021a802326f25abba19dc9d207fe1a19f
ShaiKapah <shaikapah09@gmail.com>
2025-12-27 18:23:45 +0200

    Update ex5.c

--- ex5.c
+++ ex5.c
@@ -1,91 +1,57 @@
-/***********
- ID:
- NAME:
-***********/
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef struct Episode {
-    char *name;
-    char *length;
-    struct Episode *next;
-} Episode;
-
-typedef struct Season {
-    char *name;
-    Episode *episodes;
-    struct Season *next;
-} Season;
-
-typedef struct TVShow {
-    char *name;
-    Season *seasons;
-} TVShow;
-
-TVShow ***database = NULL;
-int dbSize = 0;
-
-char *getString();
-int getInt();
-
-int validLength(char *s);
-int countShows();
-
-void shrinkDB();
-void expandDB();
-
-void freeEpisode(Episode *e);
-void freeSeason(Season *s);
-void freeShow(TVShow *show);
-void freeAll();
-
-TVShow *findShow(char *name);
-Season *findSeason(TVShow *show, char *name);
-Episode *findEpisode(Season *season, char *name);
-
-void addShow();
-void addSeason();
-void addEpisode();
-
-void deleteShow();
-void deleteSeason();
-void deleteEpisode();
-
-void printEpisode();
-void printShow();
-void printArray();
-
-void addMenu() {
-    int choice;
-    printf("Choose an option:\n");
-    printf("1. Add a TV show\n");
-    printf("2. Add a season\n");
-    printf("3. Add an episode\n");
-    scanf("%d", &choice);
-    getchar();
-    switch (choice) {
-        case 1: addShow(); break;
-        case 2: addSeason(); break;
-        case 3: addEpisode(); break;
+void deleteEpisode(){
+    printf("Enter the name of the show:\n");
+    char *showName = getString();
+    TVShow *show = findShow(showName);
+    if(!show){
+        printf("Show not found.\n");
+        free(showName);
+        return;
     }
+    printf("Enter the name of the season:\n");
+    char *seasonName = getString();
+    Season *season = findSeason(show, seasonName);
+    if(!season){
+        printf("Season not found.\n");
+        free(showName);
+        free(seasonName);
+        return;
+    }
+    printf("Enter the name of the episode:\n");
+    char *episodeName = getString();
+    Episode *episode = findEpisode(season, episodeName);
+    if(!episode){
+        printf("Episode not found.\n");
+        free(showName);
+        free(seasonName);
+        free(episodeName);
+        return;
+    }
+
+    free(showName);
+    free(seasonName);
+    Episode *curr = season->episodes;
+    Episode *prev = NULL;
+
+    while (curr != NULL && strcmp(curr->name, episodeName) != 0) {
+        prev = curr;
+        curr = curr->next;
+    }
+    free(episodeName);
+
+    if (curr == NULL) {
+        printf("Episode not found.\n");
+        return;
+    }
+    if (prev == NULL) {
+        season->episodes = curr->next;
+    } else {
+        prev->next = curr->next;
+    }
+    free(curr->name);
+    free(curr->length);
+    free(curr);
 }
 
-void deleteMenu() {
-    int choice;
-    printf("Choose an option:\n");
-    printf("1. Delete a TV show\n");
-    printf("2. Delete a season\n");
-    printf("3. Delete an episode\n");
-    scanf("%d", &choice);
-    getchar();
-    switch (choice) {
-        case 1: deleteShow(); break;
-        case 2: deleteSeason(); break;
-        case 3: deleteEpisode(); break;
-    }
-}
 
 void printMenuSub() {
     int choice;
@@ -100,28 +66,4 @@ void printMenuSub() {
         case 2: printEpisode(); break;
         case 3: printArray(); break;
     }
-}
-
-void mainMenu() {
-    printf("Choose an option:\n");
-    printf("1. Add\n");
-    printf("2. Delete\n");
-    printf("3. Print\n");
-    printf("4. Exit\n");
-}
-
-int main() {
-    int choice;
-    do {
-        mainMenu();
-        scanf("%d", &choice);
-        getchar();
-        switch (choice) {
-            case 1: addMenu(); break;
-            case 2: deleteMenu(); break;
-            case 3: printMenuSub(); break;
-            case 4: freeAll(); break;
-        }
-    } while (choice != 4);
-    return 0;
 }
